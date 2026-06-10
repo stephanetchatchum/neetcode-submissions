d93f37277b12793284c306df5eacdc328c5a4361
Stephane Tchatchum <stephanetchatchum@gmail.com>
2026-06-10 10:28:36 +0200

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const hashSet = new Set();
+        for (let i=0;i<nums.length; i++){
+            if (hashSet.has(nums[i])){
+                return true;
+            } else {
+                hashSet.add(nums[i]);
+            }
+        }
+        return false
+    }
+}
